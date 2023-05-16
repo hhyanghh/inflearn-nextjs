09bff2340011da55eedabb44cdf5bf94891f70d9
hhyanghh <yanghh711@naver.com>
2023-05-16 23:12:14 +0900

    Link, routing, SSG+CSR

---
+++ src/pages/section01/links.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function Links() {
+  return (
+    <main>
+      <h1>Links</h1>
+      <Link href="/section01/getStaticProps">/getStartProps</Link>
+    </main>
+  );
+}
